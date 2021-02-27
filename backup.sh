78f3ff2dd459342de32f4a852ac9c780a0e3659b
Niklas Sombert <niklas@ytvwld.de>
2021-02-27 18:29:47 +0100

    Honor CLICKHOUSE_HOST

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@ set -eu
 while true
 do
     DATE=$(date -I)
-    clickhouse client -q "SELECT * FROM $TABLE INTO OUTFILE '$TABLE-$DATE.csv' FORMAT CSVWithNames"
+    clickhouse client --host $CLICKHOUSE_HOST -q "SELECT * FROM $TABLE INTO OUTFILE '$TABLE-$DATE.csv' FORMAT CSVWithNames"
     bzip2 /backup/$TABLE-$DATE.csv
-    clickhouse client -q "SHOW CREATE TABLE $TABLE" | sed 's/\\n/\n/g' > /backup/$TABLE-schema-$DATE.sql
+    clickhouse client --host $CLICKHOUSE_HOST -q "SHOW CREATE TABLE $TABLE" | sed 's/\\n/\n/g' > /backup/$TABLE-schema-$DATE.sql
     sleep 24h
 done
